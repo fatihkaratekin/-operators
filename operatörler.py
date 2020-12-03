c149a5d1d80b81cfba01465522f1c7c7c3d87d34
fatihkaratekin <fatihkaratekin17@hotmail.com>
2020-12-03 11:09:03 +0300

    operators

---
+++ operatörler.py
@@ -0,0 +1,16 @@
+#AND OPERATÖRÜ
+
+deneme = (30 == 30 and 20 ==20) #iki operatörde true oldugunda true döner.
+print(deneme)
+deneme2 = (50 == 50 and 20!=20)
+print(deneme2)
+
+#OR OPERATÖRÜ
+
+deneme3 = ("fatih" == "fatih" or "mehmet" == "ahmet") #En az 1 tane dogru varsa true döner yoksa false döner.
+print(deneme3)
+
+#NOT OPERATÖRÜ
+
+deneme4 =  not ("A" == "A") #True yada False farketmez başına not metodu gelirse olan durumun tersi olur.
+print(deneme4)
